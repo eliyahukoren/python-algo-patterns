2ac65dc06459764b7613e529157b74c80d62c4ff
eliyahukoren <elik.koren@gmail.com>
2023-01-05 08:43:44 +0200

    refactoring frequency counter/same

--- test/FrequencyCounter/test_same.py
+++ test/FrequencyCounter/test_same.py
@@ -1,15 +1,16 @@
 import unittest
 from FrequencyCounter.same import FrequencyCounterSame
 
-class TestFCSame(unittest.TestCase):
-  def test_same(self):
-    fc = FrequencyCounterSame()
-    testData = [
-      [[], [], False],
-      [[1], [1], False],
-      [[2, 5, 1, 3], [25, 4, 9, 2], True],
-      [[6, 1, 6], [36, 1], False],
-      [[1, 4, 6], [16, 1, 36], True],
-    ]
-    for test in testData:
-      self.assertEqual(fc.same(test[0], test[1]), test[2]);
+
+class TestFrequencyCounterSame(unittest.TestCase):
+    def test_same(self):
+        fc = FrequencyCounterSame()
+        testData = [
+            [[], [], False],
+            [[1], [1], True],
+            [[2, 5, 1, 3], [25, 4, 9, 2], False],
+            [[6, 1, 6], [36, 1], False],
+            [[1, 4, 6], [16, 1, 36], True],
+        ]
+        for test in testData:
+            self.assertEqual(fc.same(test[0], test[1]), test[2])
